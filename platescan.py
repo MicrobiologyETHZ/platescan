05dbf744306d9d4e3de6187907c3a33b7e60e42e
cmfield <fieldc@ethz.ch>
2019-10-31 14:54:07 +0100

    version 2

--- platescan.py
+++ platescan.py
@@ -1,8 +1,7 @@
-import argparse,os,sys,shutil,scipy
+import argparse,os,scipy
 import numpy as np
 import scipy.signal
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
 from skimage import io,exposure
 from skimage.draw import circle,ellipse
 
@@ -25,82 +24,74 @@ def trisect(image):
 
     return(images)
 
-
-def cropImage(image):
-    #Plate edge detection
-    h,w = image.shape
-    rowMeans = np.zeros(h)
-    colMeans = np.zeros(w)
-    for i in range(0,h):
-        rowMeans[i] = np.mean(image[i,:])
-    for i in range(0,w):
-        colMeans[i] = np.mean(image[:,i])
-    filt = scipy.signal.gaussian(21,2)
-    rowMeans = np.pad(scipy.signal.convolve(rowMeans,filt,'valid'),10,'edge')
-    colMeans = np.pad(scipy.signal.convolve(colMeans,filt,'valid'),10,'edge')
-   
-    Lpos,Rpos = np.array_split(scipy.signal.argrelextrema(colMeans,np.greater)[0],2)
-    L = np.sort(Lpos[np.argpartition(colMeans[Lpos],-1)[-1:]])[0]
-    R = np.sort(Rpos[np.argpartition(colMeans[Rpos],-1)[-1:]])[0]
-    Tpos,Bpos = np.array_split(scipy.signal.argrelextrema(rowMeans,np.greater)[0],2)
-    T = np.sort(Tpos[np.argpartition(rowMeans[Tpos],-1)[-1:]])[0]
-    B = np.sort(Bpos[np.argpartition(rowMeans[Bpos],-1)[-1:]])[0]
+def cropImage(image,blank):
+    if blank is not None:
+        # If a blank is provided, use that to crop the image to the plate
+        xcor = crossCorrelate(image,blank)
+        offset = np.where(xcor == xcor.max())
+        h,w = blank.shape
+        L = offset[1][0]
+        R = offset[1][0]+w
+        T = offset[0][0]
+        B = offset[0][0]+h
+    else:
+        # Plate edge detection; performance highly dependent on lighting and plate type
+        h,w = image.shape
+        rowMeans = [np.mean(image[i,:]) for i in range(0,h)]
+        colMeans = [np.mean(image[:,i]) for i in range(0,w)]
+        
+        # Smooth curves
+        rowMeans = scipy.signal.savgol_filter(rowMeans,int(h/15)+1-int(h/15)%2,3)
+        colMeans = scipy.signal.savgol_filter(colMeans,int(w/15)+1-int(w/15)%2,3)
+        
+        Lpos,Rpos = np.array_split(scipy.signal.argrelextrema(colMeans,np.greater)[0],2)
+        L = np.sort(Lpos[np.argpartition(colMeans[Lpos],-1)[-1:]])[0]
+        R = np.sort(Rpos[np.argpartition(colMeans[Rpos],-1)[-1:]])[0]
+        Tpos,Bpos = np.array_split(scipy.signal.argrelextrema(rowMeans,np.greater)[0],2)
+        T = np.sort(Tpos[np.argpartition(rowMeans[Tpos],-1)[-1:]])[0]
+        B = np.sort(Bpos[np.argpartition(rowMeans[Bpos],-1)[-1:]])[0]
     
-    plt.figure("R")
-    plt.plot(rowMeans,range(0,h))
-    plt.plot(rowMeans[Tpos],Tpos,'o')
-    plt.plot(rowMeans[Bpos],Bpos,'o')
-    plt.savefig("row.png")
-
-    plt.figure("C")
-    plt.plot(colMeans)
-    plt.plot(Lpos,colMeans[Lpos],'o')
-    plt.plot(Rpos,colMeans[Rpos],'o')
-    plt.savefig("col.png")
-
-    print("Cropping plate")
-    print("  L "+str(L))
-    print("  R "+str(R))
-    print("  T "+str(T))
-    print("  B "+str(B))
-   
+    print("Cropping plate:")
+    print("{}:{} left to right".format(L,R))
+    print("{}:{} top to bottom".format(T,B))
+    
     return(image[T:B,L:R])
         
-def crossCorrelate(a,b,mode='full',boundary='fill'):
+def crossCorrelate(a,b,boundary=None):
     #Perform a normalized cross-correlation of two arrays
     
+    if boundary == 'wrap':
+        a = wrapImage(a)
     aN = (a-np.mean(a))/np.std(a)
     bN = (b-np.mean(b))/np.std(b)
-    xcor = scipy.signal.correlate2d(aN,bN,mode=mode,boundary=boundary)
+    xcor = scipy.signal.correlate(aN,bN,mode='valid',method='fft')
+    
     return(xcor)
-
-def findGrid(plate,grid=384):
-    #Make an array of 384 spots to use as a mask, cross-correlate with the plate image and find the best-fitting grid points
+    
+def findGrid(plate,nrows,ncols,layout,r,xGap,yGap,pad):
+    #Make an array of spots to use as a mask, cross-correlate with the plate image and find the best-fitting grid points
     print("  Finding colonies")
     
-    r = 5
-    pad = 25
-    xGap = 52.3
-    yGap = 53.1
-    
-    mask = np.zeros((int((2*pad)+(2*r)+(15*yGap)),int((2*pad)+(2*r)+(23*xGap))))
-    for y in range(0,16):
-        for x in range(0,24):
-            mask[circle(pad+r+round(y*yGap)-1,pad+r+round(x*xGap)-1,r)] = 1
-    
-    xcor = crossCorrelate(plate,mask,mode='valid')
+    mask = -np.ones((round((2*pad)+((nrows-1)*yGap)),round((2*pad)+((ncols-1)*xGap))))
+    for y in range(0,nrows):
+        for x in range(0,ncols):
+            if layout[y,x]:
+                mask[circle(pad+round(y*yGap),pad+round(x*xGap),r)] = 1
+
+    xcor = crossCorrelate(plate,mask)
     offset = np.where(xcor == xcor.max())
     offset = np.array((offset[0][0]+pad,offset[1][0]+pad))
     
-    rows = np.array([round(offset[0]+(y*yGap)) for y in range(0,16)])
-    cols = np.array([round(offset[1]+(x*xGap)) for x in range(0,24)])
+    rowIndex = np.array([round(offset[0]+(y*yGap)) for y in range(0,nrows)],dtype=np.int32)
+    colIndex = np.array([round(offset[1]+(x*xGap)) for x in range(0,ncols)],dtype=np.int32)
     
     masked = np.copy(plate)
-    for y in range(0,16):
-        for x in range(0,24):
-            masked[circle(offset[0]+r+round(y*yGap)-1,offset[1]+r+round(x*xGap)-1,r)] = 1
+    for y in range(0,nrows):
+        for x in range(0,ncols):
+            if layout[y,x]:
+                masked[circle(offset[0]+round(y*yGap),offset[1]+round(x*xGap),r)] = 1
 
-    return(masked,rows,cols)
+    return(masked,rowIndex,colIndex)
 
 def correctLighting(cell):
     #Correct a lighting gradient across a cell by SVD (not used, not necessary?)
@@ -125,28 +116,46 @@ def correctLighting(cell):
     corrected = cell + correction
     return(corrected)
 
-def findColony(cell):
+def wrapImage(image):
+    h,w = image.shape
+
+    q1 = image[:int(h/2),:int(w/2)]
+    q2 = image[:int(h/2),int(w/2):]
+    q3 = image[int(h/2):,:int(w/2)]
+    q4 = image[int(h/2):,int(w/2):]
+    
+    row1 = np.concatenate((q4,q3,q4,q3),axis=1)
+    row2 = np.concatenate((q2,q1,q2,q1),axis=1)
+    row3 = np.concatenate((q4,q3,q4,q3),axis=1)
+    row4 = np.concatenate((q2,q1,q2,q1),axis=1)
+    
+    wrapped = np.concatenate((row1,row2,row3,row4))
+    
+    return(wrapped)
+
+def findColony(cell,rmin,rmax):
     #Find a colony by cross-correlation of ideal circular templates of different radii to create inside and outside regions
     
     h,w = cell.shape
+    
     offsets = []
     scores = []
-    for r in np.arange(5,15.5,0.5):
-        mask = np.zeros((h,w))
-        mask[ellipse(r-1,r-1,r,r)] = 1
+    for r in np.arange(rmin,rmax,0.5):
+        mask = -np.ones((h,w))
+        mask[ellipse(int(h/2),int(w/2),r,r)] = 1
         xcor = crossCorrelate(cell,mask,boundary='wrap')
-        xcor = xcor[h-1:-(2*r),w-1:-(2*r)] #valid region
-        offset = np.unravel_index(xcor.argmax(),xcor.shape)
+        offset = np.where(xcor==xcor[int(r):int(h)-int(r),int(r):int(w)-int(r)].max())
+        offset = (offset[0][0],offset[1][0])
         offsets.append(offset)
         scores.append(xcor.max())
 
     best = scores.index(max(scores))
-    r = np.arange(5,15.5,0.5)[best]
+    r = np.arange(rmin,rmax,0.5)[best]
     offset = offsets[best]
     
     hili = np.copy(cell)
-    outside = np.ones((h,w),np.bool)
-    outside[ellipse(offset[0]+r-1,offset[1]+r-1,r,r)] = 0
+    outside = np.ones(cell.shape,np.bool)
+    outside[ellipse(offset[0],offset[1],r,r)] = 0
     hili[outside] = hili[outside]/2
     return(offset[0],offset[1],r,max(scores),hili)
 
@@ -155,7 +164,7 @@ def scoreColony(cell,r,offset):
     
     h,w = cell.shape
     outside = np.ones((h,w),np.bool)
-    outside[ellipse(offset[0]+r-1,offset[1]+r-1,r,r)] = 0    
+    outside[ellipse(offset[0],offset[1],r,r)] = 0    
     
     bg = np.mean(cell[outside])
     fg = np.mean(cell[~outside])
@@ -163,74 +172,54 @@ def scoreColony(cell,r,offset):
     fgvar = np.var(cell[~outside])
     return(bg,fg,bgvar,fgvar)
 
-def scoreGrowth(plate,rows,cols):
+def scoreGrowth(plate,rowIndex,colIndex,layout,rmin,rmax,pad):
     #Find each cvarsolony within its cell, then score it according to brightness of pixels inside vs. outside the colony
     print("  Scoring growth")
     
+    nrows = len(rowIndex)
+    ncols = len(colIndex)
+    
     h,w = plate.shape
     hilighted = np.copy(plate)
-    roffsets = np.zeros((16,24))
-    coffsets = np.zeros((16,24))
-    radii = np.zeros((16,24))
-    ccscores = np.zeros((16,24))
-    bgs = np.zeros((16,24))
-    fgs = np.zeros((16,24))
-    bgvars = np.zeros((16,24))
-    fgvars = np.zeros((16,24))
-    for rid,row in enumerate(rows):
-        for cid,col in enumerate(cols):
-            print("    Working on cell "+str(rid)+"x"+str(cid))
-            cell = plate[max(0,row-25):min(h,row+25),max(0,col-25):min(w,col+25)]
-            
-            #A lighting correction may be needed, but it's unclear if that's true and how best to implement it
-                        
-            roffsets[rid,cid],coffsets[rid,cid],radii[rid,cid],ccscores[rid,cid],hili = findColony(cell)
-            bgs[rid,cid],fgs[rid,cid],bgvars[rid,cid],fgvars[rid,cid] = scoreColony(cell,radii[rid,cid],(roffsets[rid,cid],coffsets[rid,cid]))
-            hilighted[max(0,row-25):min(h,row+25),max(0,col-25):min(w,col+25)] = hili
+    roffsets = np.zeros((nrows,ncols))
+    coffsets = np.zeros((nrows,ncols))
+    radii = np.zeros((nrows,ncols))
+    ccscores = np.zeros((nrows,ncols))
+    bgs = np.zeros((nrows,ncols))
+    fgs = np.zeros((nrows,ncols))
+    bgvars = np.zeros((nrows,ncols))
+    fgvars = np.zeros((nrows,ncols))
+    for rid,row in enumerate(rowIndex):
+        for cid,col in enumerate(colIndex):
+            if layout[rid,cid]:
+                print("    Working on cell "+str(rid)+"x"+str(cid))
+                cell = plate[max(0,row-pad):min(h,row+pad),max(0,col-pad):min(w,col+pad)]
+                roffsets[rid,cid],coffsets[rid,cid],radii[rid,cid],ccscores[rid,cid],hili = findColony(cell,rmin,rmax)
+                bgs[rid,cid],fgs[rid,cid],bgvars[rid,cid],fgvars[rid,cid] = scoreColony(cell,radii[rid,cid],(roffsets[rid,cid],coffsets[rid,cid]))
+                hilighted[max(0,row-pad):min(h,row+pad),max(0,col-pad):min(w,col+pad)] = hili
             
     return(hilighted,roffsets,coffsets,radii,ccscores,bgs,fgs,bgvars,fgvars)
 
-def formatResults(rows,cols,roffsets,coffsets,radii,ccscores,bgs,fgs,bgvars,fgvars):
-    results = np.empty(384,dtype=([('Strain','a3'),('Row','i8'),('Col','i8'),('PixelRow','i8'),('PixelCol','i8'),('Radius','f8'),('CCScore','f8'),('BgMean','f8'),('FgMean','f8'),('BgVar','f8'),('FgVar','f8')]))
-    for r in range(0,8):
-        for c in range(0,12):
-            for rpos,cpos in [(0,0),(0,1),(1,0),(1,1)]:
-                n = (((r*12)+c)*4)+((rpos*2)+cpos)
-                row,col = (2*r)+rpos,(2*c)+cpos
-                results[n]['Strain'] = chr(r+ord("A"))+str(c+1)
-                results[n]['Row'] = row
-                results[n]['Col'] = col
-                results[n]['PixelRow'] = int(max(0,rows[row]-25)+roffsets[row,col]+radii[row,col]-1)
-                results[n]['PixelCol'] = int(max(0,cols[col]-25)+coffsets[row,col]+radii[row,col]-1)
-                results[n]['Radius'] = radii[row,col]
-                results[n]['CCScore'] = ccscores[row,col]
-                results[n]['BgMean'] = bgs[row,col]
-                results[n]['FgMean'] = fgs[row,col]
-                results[n]['BgVar'] = bgvars[row,col]
-                results[n]['FgVar'] = fgvars[row,col]
+def formatResults(rowIndex,colIndex,layout,roffsets,coffsets,radii,ccscores,bgs,fgs,bgvars,fgvars):
+    results = np.empty(np.sum(layout),dtype=([('Strain','a3'),('Row','i8'),('Col','i8'),('PixelRow','i8'),('PixelCol','i8'),('Radius','f8'),('CCScore','f8'),('BgMean','f8'),('FgMean','f8'),('BgVar','f8'),('FgVar','f8')]))
+    i = 0
+    for row in range(0,len(rowIndex)):
+        for col in range(0,len(colIndex)):
+            if layout[row,col]:
+                results[i]['Strain'] = chr(row+ord("A"))+str(col+1)
+                results[i]['Row'] = row
+                results[i]['Col'] = col
+                results[i]['PixelRow'] = rowIndex[row]+roffsets[row,col]
+                results[i]['PixelCol'] = colIndex[col]+coffsets[row,col]
+                results[i]['Radius'] = radii[row,col]
+                results[i]['CCScore'] = ccscores[row,col]
+                results[i]['BgMean'] = bgs[row,col]
+                results[i]['FgMean'] = fgs[row,col]
+                results[i]['BgVar'] = bgvars[row,col]
+                results[i]['FgVar'] = fgvars[row,col]
+                i += 1
     return(results)
     
-def plotResults(radii,deltas,varias,filename):
-    #Unused, moved to R
-    pdf = PdfPages(filename)
-
-    radiiPlot = plt.figure()
-    radiiPlot.suptitle("Colony Radius")
-    plt.imshow(radii,interpolation='none')
-    radiiPlot.savefig(pdf,format='pdf')
-    
-    deltasPlot = plt.figure()
-    deltasPlot.suptitle("Mean FG - BG")
-    plt.imshow(deltas,interpolation='none')
-    deltasPlot.savefig(pdf,format='pdf')
-
-    variasPlot = plt.figure()
-    variasPlot.suptitle("Variance")
-    plt.imshow(varias,interpolation='none')
-    variasPlot.savefig(pdf,format='pdf')
-    
-    pdf.close()
-
 def outputResults(results,filename):
     fo = open(filename,'w')
     for result in results:
@@ -240,19 +229,51 @@ def outputResults(results,filename):
     fo.close()
 
 ### MAIN ###
-#def __main__():
-if True:
+def __main__():
     parser = argparse.ArgumentParser(description='Assessing colony growth on arrayed plates.')
     parser.add_argument('file',metavar='image_file',help='Image file of colonies arrayed on a plate(s)')
     parser.add_argument('-t','--three',action='store_true',help='Image contains three plates as repeats')
     parser.add_argument('-s','--scan',action='store_true',help='Image was scanned, not photographed')
+    parser.add_argument('-b','--blank',metavar='blank_file',help='Image file of a blank plate')
+    parser.add_argument('-l','--layout',metavar='layout_file',help='Layout of colonies on the plate')
+    parser.add_argument('-r','--radius',metavar='colony_radius',help='Approximate radius of the colonies in pixels',type=int)
+    parser.add_argument('--min_r',metavar='min_radius',help='Minimum radius of the colonies in pixels',type=int)
+    parser.add_argument('--max_r',metavar='max_radius',help='Maximum radius of the colonies in pixels',type=int)
+    parser.add_argument('-x','--xgap',metavar='xgap',help='Horizontal gap between colony centres in pixels',type=int)
+    parser.add_argument('-y','--ygap',metavar='ygap',help='Vertical gap between colony centres in pixels',type=int)
+    parser.add_argument('-p','--pad',metavar='pad',help='Area to search outside of colony centres in pixels',type=int)
     parser.add_argument('-o','--output',metavar='output_prefix',help='Prefix for output files')
 
     args = parser.parse_args()
 
+    # Default arguments
+    if args.blank is not None:
+        blank = io.imread(args.blank,as_gray=True)
+    else:
+        blank = None
+    if args.radius is None:
+        args.radius = 30
+    if args.xgap is None:
+        args.xgap = 100
+    if args.ygap is None:
+        args.ygap = 100
+    if args.pad is None:
+        args.pad = args.xgap/2
+    if args.min_r is None:
+        args.min_r = args.radius/2
+    if args.max_r is None:
+        args.max_r = args.radius*2
+
     #Get image name and path for output files
     if args.output is None:
         args.output = os.path.splitext(args.file)[0]
+
+    # Import layout file and determine the number of rows and columns
+    if args.layout is not None:
+        layout = np.loadtxt(args.layout,dtype='i')
+    else:
+        layout = np.ones((8,12))
+    nrows,ncols = layout.shape
 
     #Flip, invert, convert to grayscale and normalise levels to between 1% and 99%
     image = io.imread(args.file,as_gray=True)
@@ -262,30 +283,23 @@ if True:
     image = exposure.rescale_intensity(image,(p1,p99))
     h,w = image.shape
 
-    #Thresholds for growth
-    deltaThreshold = 0.1
-    zscoreThreshold = 2
-
     if args.three:
-        plates = trisect(image,option)
+        plates = trisect(image)
     else:
         plates = [image]
 
     for pid,plate in enumerate(plates):
         print("Plate "+str(pid+1)+":")
-#        plate = cropImage(plate)
-        io.imsave("{}_{}_cut.png".format(args.output,pid),plate)
-        interrupt
-        masked,rows,cols = findGrid(plate)
+        cropped = cropImage(image,blank)
+        io.imsave("{}_{}_cut.png".format(args.output,pid),cropped)
+        masked,rowIndex,colIndex = findGrid(cropped,nrows,ncols,layout,r=args.radius,xGap=args.xgap,yGap=args.ygap,pad=args.pad)
         io.imsave("{}_{}_mask.png".format(args.output,pid),masked)
-        hilighted,roffsets,coffsets,radii,ccscores,bgs,fgs,bgvars,fgvars = scoreGrowth(plate,rows,cols)
+        hilighted,roffsets,coffsets,radii,ccscores,bgs,fgs,bgvars,fgvars = scoreGrowth(cropped,rowIndex,colIndex,layout,rmin=args.min_r,rmax=args.max_r,pad=args.pad)
         io.imsave("{}_{}_hili.png".format(args.output,pid),hilighted)
-        results = formatResults(rows,cols,roffsets,coffsets,radii,ccscores,bgs,fgs,bgvars,fgvars)
-        plotResults(radii,deltas,varias,"{}_{}.pdf".format(args.output,pid))
-        outputResults(results,imDir,"{}_{}.txt".format(args.output,pid))
+        results = formatResults(rowIndex,colIndex,layout,roffsets,coffsets,radii,ccscores,bgs,fgs,bgvars,fgvars)
+        outputResults(results,"{}_{}.txt".format(args.output,pid))
 
-#    return(0)
+    return()
 
-#if __name__ == "__main__":
-#    __main__()
-
+if __name__ == "__main__":
+    __main__()
